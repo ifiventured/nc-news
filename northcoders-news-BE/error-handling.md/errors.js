6cef5cdfc9b260b0079d8a86aac184b07e93f850
ifiventured <cjtestmember01@gmail.com>
2026-01-06 14:14:49 +0000

    ready for FE

---
+++ northcoders-news-BE/error-handling.md/errors.js
@@ -0,0 +1,23 @@
+exports.handleNotFound = (req, res) => {
+    res.status(404).send({ msg: "Path not found" });
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+    // invalid input syntax (e.g. /api/articles/not-a-number)
+    if (err.code === "22P02") {
+        return res.status(400).send({ msg: "Bad request" });
+    }
+    next(err);
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+        return res.status(err.status).send({ msg: err.msg });
+    }
+    next(err);
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+    console.error(err);
+    res.status(500).send({ msg: "Internal server error" });
+};
