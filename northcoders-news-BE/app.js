d74765f167175758f887afe8b21dabbd9b35995d
ifiventured <cjtestmember01@gmail.com>
2026-01-08 15:41:05 +0000

    comments and votes added

--- northcoders-news-BE/app.js
+++ northcoders-news-BE/app.js
@@ -1,47 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+
+const { getTopics } = require("./controllers/topicsController");
+
+const {
+    getArticles,
+    getArticleById,
+    patchArticleVotes,
+} = require("./controllers/articlesController");
+
 const { getCommentsByArticleId } = require("./controllers/commentsController");
 
-// controllers
-const { getTopics } = require("./controllers/topicsController");
-const { getArticles, getArticleById } = require("./controllers/articlesController");
-
-// error handlrs
 const {
+    handleNotFound,
     handlePsqlErrors,
     handleCustomErrors,
     handleServerErrors,
-    handleNotFound,
 } = require("./errors/errors");
 
 const app = express();
 
-//middleware
 app.use(cors());
 app.use(express.json());
 
-// api root
-app.get("/api", (req, res) => {
-    res.status(200).send({
-        endpoints: {
-            "GET /api": "list endpoints",
-            "GET /api/topics": "get topics",
-            "GET /api/articles": "get articles",
-            "GET /api/articles/:article_id": "get article by id",
-        },
-    });
-});
-
-// routes
+// ROUTES
 app.get("/api/topics", getTopics);
+
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
+app.patch("/api/articles/:article_id", patchArticleVotes);
+
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
-
-
-// errors
 app.all(/.*/, handleNotFound);
+
+// error handlers
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
--- northcoders-news-BE/controllers/articlesController.js
+++ northcoders-news-BE/controllers/articlesController.js
@@ -1,22 +1,38 @@
 const {
     selectArticles,
-    selectArticleById
+    selectArticleById,
+    updateArticleVotes,
 } = require("../models/articlesModel");
 
 exports.getArticles = (req, res, next) => {
     selectArticles()
-        .then((articles) => res.status(200).send({ articles }))
+        .then((articles) => {
+            res.status(200).send({ articles });
+        })
         .catch(next);
 };
 
 exports.getArticleById = (req, res, next) => {
     const { article_id } = req.params;
 
-    if (isNaN(article_id)) {
-        return res.status(400).send({ msg: "Invalid article_id" });
+    selectArticleById(article_id)
+        .then((article) => {
+            res.status(200).send({ article });
+        })
+        .catch(next);
+};
+
+exports.patchArticleVotes = (req, res, next) => {
+    const { article_id } = req.params;
+    const { inc_votes } = req.body;
+
+    if (typeof inc_votes !== "number") {
+        return res.status(400).send({ msg: "Bad request" });
     }
 
-    selectArticleById(article_id)
-        .then((article) => res.status(200).send({ article }))
+    updateArticleVotes(article_id, inc_votes)
+        .then((article) => {
+            res.status(200).send({ article });
+        })
         .catch(next);
 };
